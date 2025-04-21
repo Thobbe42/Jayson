ca9d378b641e8f1703d758671057c1397e14480b
Thorben Linker <thorbenlinker@gmail.com>
2025-04-21 08:42:48 +0200

    feat: implement parsing logic

--- lib/src/main/java/com/thobbe/jayson/Parser.java
+++ lib/src/main/java/com/thobbe/jayson/Parser.java
@@ -1,8 +1,6 @@
 package com.thobbe.jayson;
 
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Parser {
 
@@ -16,6 +14,9 @@ public class Parser {
 
   private void advance() {
     currentToken = tokens.poll();
+    if (currentToken == null || currentToken.type().equals(TokenType.ERROR)) {
+      throw new RuntimeException("Unexpected end of file or error");
+    }
   }
 
   private void expect(TokenType expected) {
@@ -35,34 +36,91 @@ public class Parser {
   }
 
   public Object parse() {
-    return null;
+    Object value = parseValue();
+    if (!currentToken.type().equals(TokenType.EOF)) {
+      throw new RuntimeException("Unexpected additional data after JSON input");
+    }
+    return value;
   }
 
   private Object parseValue() {
-    return null;
+    return switch (currentToken.type()) {
+      case L_BRACE -> parseObject();
+      case L_BRACKET -> parseArray();
+      case STRING -> parseString();
+      case NUMBER -> parseNumber();
+      case BOOLEAN -> parseBoolean();
+      case NULL -> parseNull();
+      default -> throw new RuntimeException("Unexpected token type: " + currentToken.type());
+    };
   }
 
   private Map<String, Object> parseObject() {
-    return null;
+    expect(TokenType.L_BRACE);
+
+    Map<String, Object> object = new HashMap<>();
+
+    while (match(TokenType.STRING)) {
+      String key = parseString();
+      expect(TokenType.COLON);
+      Object value = parseValue();
+
+      object.put(key, value);
+    }
+
+    expect(TokenType.R_BRACE);
+    return object;
   }
 
   private List<Object> parseArray() {
-    return null;
+    expect(TokenType.L_BRACKET);
+    List<Object> array = new ArrayList<>();
+
+    if (!match(TokenType.R_BRACKET)) {
+      do {
+        array.add(parseValue());
+      } while (match(TokenType.COMMA));
+    }
+
+    expect(TokenType.R_BRACKET);
+    return array;
   }
 
   private String parseString() {
-    return null;
+    String value = currentToken.value();
+    advance();
+    return value;
   }
 
   private Number parseNumber() {
-    return null;
+    String value = currentToken.value();
+    if (value.contains(".") || value.contains("e") || value.contains("E")) {
+      return Double.valueOf(value);
+    } else {
+      return Long.valueOf(value);
+    }
   }
 
   private Boolean parseBoolean() {
-    return null;
+    if (currentToken.value().equals("true")) {
+      advance();
+      return Boolean.TRUE;
+    } else if (currentToken.value().equals("false")) {
+      advance();
+      return Boolean.FALSE;
+    } else {
+      throw new RuntimeException(
+          "Unexpected value: expected " + TokenType.BOOLEAN + "but got " + currentToken.value());
+    }
   }
 
   private Object parseNull() {
-    return null;
+    if (currentToken.value().equals("null")) {
+      advance();
+      return null;
+    } else {
+      throw new RuntimeException(
+          "Unexpected value: expected " + TokenType.NULL + "but got " + currentToken.value());
+    }
   }
 }
